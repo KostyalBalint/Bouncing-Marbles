487377eaa8866429df1e3cae4203a618a3b8cc72
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-04-17 14:51:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// module aliases
+var Engine = Matter.Engine,
+    Body = Matter.Body,
+    Events = Matter.Events,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+window.onload = () => {
+
+  // create an engine
+  var engine = Engine.create(document.getElementById("canvas-container"));
+
+  // create a renderer
+  var render = Render.create({
+      element: document.getElementById("canvas-container"),
+      engine: engine,
+  });
+
+  //Create ground and divider
+  var ground = Body.create({
+    parts: [Bodies.rectangle(400, 150, 810, 200), Bodies.rectangle(400, -75, 20, 300)],
+    isStatic: true
+  });
+  // add all of the bodies to the world
+  Composite.add(engine.world, [ground]);
+
+  var objects = [];
+  var r = 5;
+
+  for (var i = 0; i < 20; i++) {
+    for (var j = 0; j < 20; j++) {
+      objects.push(Bodies.circle(50 + i*r*2, 50 + j*r*2, r));
+    }
+  }
+  Composite.add(engine.world, objects);
+
+  //Create walls
+  Composite.add(engine.world, [
+      Bodies.rectangle(400, -25, 800, 100, { isStatic: true }),
+      Bodies.rectangle(400, 625, 800, 100, { isStatic: true }),
+      Bodies.rectangle(825, 300, 100, 600, { isStatic: true }),
+      Bodies.rectangle(-25, 300, 100, 600, { isStatic: true })
+  ]);
+
+  // run the renderer
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+
+  // run the engine
+  Runner.run(runner, engine);
+
+  setFreq(1);
+  Events.on(engine, 'beforeUpdate', (event) =>{
+    var py = 500+ 50 * Math.sin(engine.timing.timestamp * (window.freq / 1000));
+    Body.setVelocity(ground, { x: 0, y: py - ground.position.y });
+    Body.setPosition(ground, { x: ground.position.x, y: py });
+  });
+
+};
+
+function setFreq(freq){
+  window.freq = freq;
+}
